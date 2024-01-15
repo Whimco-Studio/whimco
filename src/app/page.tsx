22bc5f6b0a8c03b4f5b9a85bf1d3b608f347249e
TrendonRobinson <trendonrobinson@gmail.com>
2024-01-15 15:27:31 -0500

    - Site build

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center p-24 bg-custom-gradient">
+			{/* Centered image container */}
+			<div className="flex justify-center items-center w-full max-w-screen-xl mx-auto">
+				<Image
+					src="/White.png"
+					alt="Whimco"
+					width={1200}
+					height={100}
+					layout="intrinsic"
+				/>
+			</div>
+
+			<h1 className="text-resize mt-20">Turning Whims into Wonders</h1>
+		</main>
+	);
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"custom-gradient":
+					"linear-gradient(45deg, rgba(255,134,255,1) 0%, rgba(132,134,240,1) 50%, rgba(0,255,255,1) 100%)",
+
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
